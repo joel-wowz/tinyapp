1fc7fe7b353200253eb02d706a4397b94ba211db
joel-wowz <josephlafluer@hotmail.com>
2020-02-18 02:32:17 +0000

    urlshortpart1

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
-
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,3 +20,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: "b2xVn2", longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
