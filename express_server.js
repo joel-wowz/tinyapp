43c9acf2f3eb3b88b8f18a4d3920699e21876b99
joel-wowz <josephlafluer@hotmail.com>
2020-02-20 03:39:10 +0000

    commit test

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require(`cookie-parser`)
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 function generateRandomString() {
   let randomString = "";
   for (let i = 0; i <= 6; i++) {
@@ -24,31 +26,27 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) =>{
-  res.send("<html><body> Hello <b>HOMIES</b></body></html>\n");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls : urlDatabase };
+  let templateVars = { username: req.cookies["username"], urls : urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+    
+  };
+  res.render("urls_new", templateVars);
 });
-/* app.post("/urls/new", (req, res) =>{
-  console.log(req.params);
-  console.log(req.params.body)
-})
- */
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 
 });
@@ -75,9 +73,24 @@ app.post("/urls/:shortURL/delete", (req, res)=>{
 
 app.post("/urls/:id", (req, res) => {
   let shortURL =  req.params.id;
-  console.log('shortURL:', shortURL);
-  console.log('longURL:', req.body.longURL);
   urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls`);
+});
+
+app.post("/login", (req, res) => {
+
+  const username = req.body.username
+  res.cookie(`username`,username)
+  res.redirect(`/urls`)
+  let templateVars = {
+    username: req.cookies["username"],
+    
+  };
+  res.render("urls_index", templateVars)
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie(req.cookies["username"]);
   res.redirect(`/urls`)
 });
 
